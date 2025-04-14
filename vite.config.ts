cb75af4198ff9368a3a23eff17ed6d28b9459d88
Richard Powell <richard.powell@shopify.com>
2025-04-14 14:16:47 -0400

    Future flag: Remove install globals as per https://remix.run/docs/en/main/start/future-flags\#remove-installglobals

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
 import { defineConfig, type UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-installGlobals({ nativeFetch: true });
 
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
