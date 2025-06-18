954da05b5ab5f7d91adbbdaf1a2d602e098fc6e4
Richard Powell <richard.powell@shopify.com>
2025-06-18 16:48:58 -0400

    Shopify App: Convert @shopify/polaris UI to Polaris web components.  Remove @shopify/polaris

--- app/routes/auth.login/route.tsx
+++ app/routes/auth.login/route.tsx
@@ -2,14 +2,6 @@ import { useState } from "react";
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "react-router";
 import { Form, useActionData, useLoaderData } from "react-router";
 import { AppProvider } from "@shopify/shopify-app-react-router/react";
-import {
-  Button,
-  Card,
-  FormLayout,
-  Page,
-  Text,
-  TextField,
-} from "@shopify/polaris";
 
 import { login } from "../../shopify.server";
 import { loginErrorMessage } from "./error.server";
@@ -36,28 +28,23 @@ export default function Auth() {
 
   return (
     <AppProvider embedded={false}>
-      <Page>
-        <Card>
-          <Form method="post">
-            <FormLayout>
-              <Text variant="headingMd" as="h2">
-                Log in
-              </Text>
-              <TextField
-                type="text"
-                name="shop"
-                label="Shop domain"
-                helpText="example.myshopify.com"
-                value={shop}
-                onChange={setShop}
-                autoComplete="on"
-                error={errors.shop}
-              />
-              <Button submit>Log in</Button>
-            </FormLayout>
-          </Form>
-        </Card>
-      </Page>
+      <s-page>
+        <Form method="post">
+          <s-section heading="Log in">
+            <s-text-field
+              type="text"
+              name="shop"
+              label="Shop domain"
+              helpText="example.myshopify.com"
+              value={shop}
+              onChange={setShop}
+              autoComplete="on"
+              error={errors.shop}
+            ></s-text-field>
+            <s-button submit>Log in</s-button>
+          </s-section>
+        </Form>
+      </s-page>
     </AppProvider>
   );
 }
