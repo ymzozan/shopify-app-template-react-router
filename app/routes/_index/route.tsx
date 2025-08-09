73d14436734469ed51430ee693082c5af073f067
samcyn <samsoniyanda@outlook.com>
2025-08-10 00:04:12 +0100

    refactor: combine react-router imports into a single statement

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,6 +1,5 @@
 import type { LoaderFunctionArgs } from "react-router";
-import { redirect } from "react-router";
-import { Form, useLoaderData } from "react-router";
+import { redirect, Form, useLoaderData } from "react-router";
 
 import { login } from "../../shopify.server";
 
