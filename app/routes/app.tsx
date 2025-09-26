e29e631f1e5d725a6483e6e39e53ef882cad0c9f
Richard Powell <richard.powell@shopify.com>
2025-09-26 10:36:35 -0400

    Remove rel=home

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -18,7 +18,7 @@ export default function App() {
   return (
     <AppProvider embedded apiKey={apiKey}>
       <s-app-nav>
-        <s-link href="/app" rel="home">Home</s-link>
+        <s-link href="/app">Home</s-link>
         <s-link href="/app/additional">Additional page</s-link>
       </s-app-nav>
       <Outlet />
